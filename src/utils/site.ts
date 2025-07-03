c62394a09183aa2096cb55009a65a917ef45f99f
imtaludke <imtaludke@gmail.com>
2025-07-04 05:54:29 +0700

    Delete src directory

--- src/utils/site.ts
+++
@@ -1,4 +0,0 @@
-export const nama = 'Bocil Jepang';
-export const desk = 'Video Bokep Indo Bocil Ngentot Jilbab Adik Cewek Smp Mama Live Viral Colmek Sma Hijab Tante China';
-export const url = 'https://bociljepang.pages.dev';
-export const terbit = '2025-01-26T00:01:03Z';
